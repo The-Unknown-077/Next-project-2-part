50254062ac0c2c75558e8028109b52f65a4629bc
The-Unknown-077 <gamergamervich@gmail.com>
2025-09-17 22:59:00 +0500

    Deployment bug fixed

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,4 @@
 'use client'
-import Link from 'next/link';
 import { memo, useState } from 'react';
 import xbtn from "@/assets/Frame.svg";
 import Image from 'next/image';
--- src/components/newArrivals/ButtonView.tsx
+++ src/components/newArrivals/ButtonView.tsx
@@ -1,17 +1,10 @@
 "use client"
-import { useSearchParams, useRouter } from 'next/navigation';
 import { memo } from 'react';
 
 const ButtonView = () => {
-    const searchParams = useSearchParams();
-    const router = useRouter();
 
     const handleClick = () => {
-        const currentLimit = Number(searchParams.get("limit"));
-        const newLimit = currentLimit > 0 ? "4" : "0";
-        const params = new URLSearchParams(searchParams.toString());
-        params.set("limit", newLimit);
-        router.push(`?${params.toString()}`);
+        console.log(":(");
     };
 
     return (
@@ -20,7 +13,7 @@ const ButtonView = () => {
                 className='bg-white text-black py-[16px] px-[80px] rounded-[62px] mb-[64px] border border-black/[0.1] duration-300 hover:bg-[#f7f7f7] active:scale-[0.97]'
                 onClick={handleClick}
             >
-                {Number(searchParams.get("limit")) > 0 ? "Show Less" : "Show All"}
+                Show All
             </button>
         </div>
     );
